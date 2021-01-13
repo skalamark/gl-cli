466bece104d36e5c0aa27b8c2c8304b8d9c1e5c8
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-01-13 17:22:27 -0300

    create enum and impl: GLanguageSubCommand

--- src/flags.rs
+++ src/flags.rs
@@ -1,1 +1,12 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
+
+#[derive(Clone, Debug)]
+pub enum GLanguageSubCommand {
+	Repl,
+}
+
+impl Default for GLanguageSubCommand {
+	fn default() -> Self {
+		Self::Repl
+	}
+}
