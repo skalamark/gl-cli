417dd72cf8068741cb561cf5f75980e9dc0c6a36
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-01-13 17:20:11 -0300

    create fns: (create_app, repl_subcommand)

--- src/cli.rs
+++ src/cli.rs
@@ -1,1 +1,25 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
+
+use clap::crate_version;
+use clap::{App, SubCommand};
+
+const USAGE: &str = "
+gl [SUBCOMMAND]
+
+To start the REPL:
+    gl
+";
+
+pub fn create_app<'a, 'b>() -> App<'a, 'b> {
+	clap::App::new("gl")
+		.bin_name("gl")
+		.version(crate_version!())
+		.long_version(crate_version!())
+		.about("Interface de linha de comando para utilização da linguagem de script GLanguage")
+		.usage(USAGE.trim())
+		.subcommand(repl_subcommand())
+}
+
+fn repl_subcommand<'a, 'b>() -> App<'a, 'b> {
+	SubCommand::with_name("repl").about("Read Eval Print Loop")
+}
