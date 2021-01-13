7721a5561946f689d84b60d5652ec5dd19415277
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-01-13 17:24:21 -0300

    create struct and impl: Flags

--- src/flags.rs
+++ src/flags.rs
@@ -1,5 +1,8 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
 
+use crate::cli::create_app;
+use clap::{App, ArgMatches};
+
 #[derive(Clone, Debug)]
 pub enum GLanguageSubCommand {
 	Repl,
@@ -8,5 +11,32 @@ pub enum GLanguageSubCommand {
 impl Default for GLanguageSubCommand {
 	fn default() -> Self {
 		Self::Repl
+	}
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct Flags {
+	pub argv: Vec<String>,
+	pub subcommand: GLanguageSubCommand,
+	pub repl: bool,
+}
+
+impl Flags {
+	pub fn from_args_vec(args: Vec<String>) -> clap::Result<Flags> {
+		let app: App = create_app();
+		let matches: ArgMatches = app.get_matches_from_safe(args).map_err(|e| clap::Error {
+			message: e.message.trim_start_matches("error: ").to_string(),
+			..e
+		})?;
+
+		let mut flags: Flags = Self::default();
+
+		if let Some(m) = matches.subcommand_matches("repl") {
+			repl_parse(&mut flags, m);
+		} else {
+			repl_parse(&mut flags, &matches);
+		}
+
+		Ok(flags)
 	}
 }
