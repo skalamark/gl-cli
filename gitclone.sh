a36a0920ae68994e408fe17352c9e1112f459636
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-02-20 20:07:45 -0300

    file build

--- gitclone.sh
+++ gitclone.sh
@@ -3,3 +3,7 @@ git clone -b develop https://github.com/skalamark/gl-core.git
 git clone -b develop https://github.com/skalamark/gl-runtime.git
 git clone -b develop https://github.com/skalamark/gl-std.git
 
+cd gl
+cargo build
+echo "Usage 'gl'"
+
