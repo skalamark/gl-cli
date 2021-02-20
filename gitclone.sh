f44af946f4e4e432f627a5a211c9d724b8bcd19f
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-02-20 20:06:34 -0300

    l

---
+++ gitclone.sh
@@ -0,0 +1,5 @@
+git clone -b develop https://github.com/skalamark/gl.git
+git clone -b develop https://github.com/skalamark/gl-core.git
+git clone -b develop https://github.com/skalamark/gl-runtime.git
+git clone -b develop https://github.com/skalamark/gl-std.git
+
