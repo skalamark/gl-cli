fac591fe8b84b54746643f7a545ad5e176177477
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-03-30 14:15:33 -0300

    reformat and update all

--- src/tools/eval.rs
+++ src/tools/eval.rs
@@ -1,25 +1,18 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
 
 use gl_core::preludes::*;
-use gl_core::state::ProgramState;
 use gl_runtime::preludes::*;
 
-pub fn run(source: String, module: &String, program: &mut ProgramState) -> Result<(), AnyError> {
-	let source: Source = Source::from_string(source).unwrap();
-	let lexer: Lexer = Lexer::new(source, module);
-	let parser: Parser = Parser::new(lexer);
-	let runtime: Runtime =
-		Runtime::from_env(Rc::clone(&program.env.modules[module]), module.clone());
-	let object: Object = match runtime.run_with_parser(parser) {
-		Ok(object) => object,
-		Err(exception) => {
-			eprintln!("{}", exception);
-			return Ok(());
-		}
-	};
+pub fn run<T: Into<String>>(source: T, module: T, program: &mut ProgramState) -> crate::ResultCli {
+	let module: String = module.into();
+	let source: Source = Source::from_string(source.into());
+	let lexer: Lexer = Lexer::new(source, &module);
+	let parser: Parser = Parser::new(lexer)?;
+	let runtime: Runtime = Runtime::from_env(Rc::clone(&program.modules[&module]), &module);
+	let object: Object = runtime.run_with_parser(parser)?;
 
 	match object {
-		Object::Null => {}
+		Object::Null => {},
 		object => println!("{}", object),
 	}
 
--- src/tools/repl.rs
+++ src/tools/repl.rs
@@ -2,20 +2,18 @@
 
 use clap::crate_version;
 use gl_core::preludes::*;
-use gl_core::state::ProgramState;
 use gl_runtime::preludes::*;
-use rustyline::{error::ReadlineError, Cmd, Editor, KeyEvent, Modifiers};
+use rustyline::error::ReadlineError;
+use rustyline::{Cmd, Editor, KeyEvent, Modifiers};
 
-pub fn run(module: &String, program: &mut ProgramState) -> Result<(), AnyError> {
+pub fn run<T: Into<String>>(module: T, program: &mut ProgramState) -> crate::ResultCli {
+	let module: String = module.into();
 	let mut editor: Editor<()> = Editor::<()>::new();
 	editor.load_history(".gl_history").unwrap_or(());
 
 	editor.bind_sequence(KeyEvent::new('\r', Modifiers::NONE), Cmd::Newline);
 	editor.bind_sequence(KeyEvent::alt('\r'), Cmd::AcceptLine);
-	editor.bind_sequence(
-		KeyEvent::new('\t', Modifiers::NONE),
-		Cmd::Insert(1, format!("\t")),
-	);
+	editor.bind_sequence(KeyEvent::new('\t', Modifiers::NONE), Cmd::Insert(1, format!("\t")));
 
 	println!("GL {}", crate_version!());
 	println!("exit using ctrl+d");
@@ -26,35 +24,31 @@ pub fn run(module: &String, program: &mut ProgramState) -> Result<(), AnyError> 
 			Ok(source) => {
 				editor.add_history_entry(source.clone());
 				editor.save_history(".gl_history").unwrap_or(());
-				let source: Source = Source::from_string(source).unwrap();
-				let lexer: Lexer = Lexer::new(source, module);
-				let parser: Parser = Parser::new(lexer);
+
+				let source: Source = Source::from_string(source);
+				let lexer: Lexer = Lexer::new(source, &module);
+				let parser: Parser = Parser::new(lexer)?;
 				let runtime: Runtime =
-					Runtime::from_env(Rc::clone(&program.env.modules[module]), module.clone());
+					Runtime::from_env(Rc::clone(&program.modules[&module]), &module);
 				let object: Object = match runtime.run_with_parser(parser) {
 					Ok(object) => object,
 					Err(exception) => {
 						eprintln!("{}", exception);
 						continue;
-					}
+					},
 				};
 
 				match object {
-					Object::Null => {}
+					Object::Null => {},
 					object => println!("{}", object),
 				}
-			}
+			},
 			Err(ReadlineError::Interrupted) => {
 				println!("exit using ctrl+d");
 				continue;
-			}
-			Err(ReadlineError::Eof) => {
-				break;
-			}
-			Err(err) => {
-				eprintln!("Error: {:?}", err);
-				break;
-			}
+			},
+			Err(ReadlineError::Eof) => break,
+			Err(err) => break eprintln!("Error: {}", err),
 		}
 	}
 
